c4596f90a597ba1d7ea09db7d71a2ee0bdf56fcd
jasonTelanoff <jason.telanoff@gmail.com>
2020-05-23 22:26:28 -0700

    Created basic UI for searching

--- lib/pages/search_therapists_page.dart
+++ lib/pages/search_therapists_page.dart
@@ -1,10 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:therapize/components/platform_widgets/platform_app_bar.dart';
+import 'package:therapize/components/platform_widgets/platform_scaffold.dart';
 
-class SearchTherapistsPage extends StatelessWidget {
+class SearchTherapistsPage extends StatefulWidget {
+  @override
+  _SearchTherapistsPageState createState() => _SearchTherapistsPageState();
+}
+
+class _SearchTherapistsPageState extends State<SearchTherapistsPage> {
+  String output;
+
+  @override
+  void initState() {
+    super.initState();
+    output = "";
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return PlatformScaffold(
+      appBar: PlatformAppBar(
+        title: 'Search Therapists',
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: <Widget>[
+            Container(
+              width: MediaQuery.of(context).size.width * .9,
+              child: TextField(
+                onChanged: (v) {
+                  setState(() {
+                    output = v;
+                  });
+                },
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
